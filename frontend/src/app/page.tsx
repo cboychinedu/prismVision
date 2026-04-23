5f3ef7a960132fdb8faf68953596b1eb2862d8ef
Mbonu Chinedum <cboy.chinedu@gmail.com>
2026-04-23 21:37:26 +0100

    added the header comments

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,22 @@
+/* =================================================================
+ * Author: Engr. Mbonu Chinedum 
+ * Date Created: 2026-04-17 6:38:42 AM 
+ * Date Modified: 2026-04-17 8:14:35 PM
+ * Project Name: Prism Vision
+ * Location: Nigeria (Tinubu Administration)
+ * * LICENSE: Creative Commons Attribution-NonCommercial 4.0 (CC BY-NC)
+ * =================================================================
+ * This software is provided as Open Source for educational and 
+ * personal use. You are free to:
+ * - SHARE: Copy and redistribute the material in any medium.
+ * - ADAPT: Remix, transform, and build upon the material.
+ *
+ * UNDER THE FOLLOWING TERMS:
+ * - ATTRIBUTION: You must give appropriate credit to the author.
+ * - NON-COMMERCIAL: You may NOT use the material for commercial 
+ * purposes. This software cannot be sold or used for profit.
+ * ================================================================= */
+
 "use client";
 
 // Importing the necessary modules 
